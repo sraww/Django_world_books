1ff8c2ee65fb7f2c1cf7693d7f117a922e410e13
NR <95234863+sraww@users.noreply.github.com>
2024-08-01 17:55:46 +0300

    Update wsgi.py
    fix

--- WebBooks/wsgi.py
+++ WebBooks/wsgi.py
@@ -9,5 +9,4 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebBooks.settings')
-
 application = get_wsgi_application()
