32db6af9a3ee89c5033e7344495e1b16b93ca229
NR <95234863+sraww@users.noreply.github.com>
2024-08-07 16:33:25 +0300

    Update settings.py
    Добавление различных путей http

--- WebBooks/settings.py
+++ WebBooks/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Выбор пути http
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
