cf0ecbb4cd7f0f792d76dc35e89c936f19d1ffc0
NR <95234863+sraww@users.noreply.github.com>
2024-08-07 16:35:29 +0300

    Update urls.py
    fix

--- WebBooks/urls.py
+++ WebBooks/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='flags'),
+    #url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='flags'),
 ]
 from django.urls import path, include
 
