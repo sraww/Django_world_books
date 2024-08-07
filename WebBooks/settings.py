318fb9730c33c258f0304ba30d78e9dbfacf28f3
NR <95234863+sraww@users.noreply.github.com>
2024-08-07 16:29:18 +0300

    Update settings.py
    fix

--- WebBooks/settings.py
+++ WebBooks/settings.py
@@ -16,8 +16,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
