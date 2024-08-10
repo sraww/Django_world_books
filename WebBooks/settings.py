d5fb79e00e144552ce6012fa194864854ec0f4e4
NR <95234863+sraww@users.noreply.github.com>
2024-08-10 08:43:45 +0300

    Update settings.py
    update

--- WebBooks/settings.py
+++ WebBooks/settings.py
@@ -73,7 +73,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -88,7 +87,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
