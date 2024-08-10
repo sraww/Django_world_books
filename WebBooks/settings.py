0caf29ac7820592a3603f9c6a5af750dacbfb828
NR <95234863+sraww@users.noreply.github.com>
2024-08-10 08:42:18 +0300

    Update settings.py
    fix

--- WebBooks/settings.py
+++ WebBooks/settings.py
@@ -21,7 +21,6 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-
 ALLOWED_HOSTS = []
 
 # Application definition
