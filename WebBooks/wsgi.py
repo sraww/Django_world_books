4a9319e3824c3df255e31e617c4057772b77c590
NR <95234863+sraww@users.noreply.github.com>
2024-08-10 08:42:00 +0300

    Update wsgi.py
    fix

--- WebBooks/wsgi.py
+++ WebBooks/wsgi.py
@@ -1,6 +1,5 @@
 """
 WSGI config for WebBooks project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
