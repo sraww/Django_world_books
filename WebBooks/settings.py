b9e1cf776b8ba6610121beb10e2dc7f2b884cba6
NR <95234863+sraww@users.noreply.github.com>
2024-08-10 08:43:31 +0300

    Update settings.py
    fix

--- WebBooks/settings.py
+++ WebBooks/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -22,7 +21,6 @@ DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 ALLOWED_HOSTS = []
 # Application definition
-
 INSTALLED_APPS = [
     'catalog',
     'django.contrib.admin',
